2d2070a7dd138e00902b9e8c07f998b09b54ff28
Alex <nerdonator99@gmail.com>
2018-01-10 15:56:38 -0700

    Initial Commit

---
+++ src/org/usfirst/frc/team2449/robot/subsystems/DriveTrain.java
@@ -0,0 +1,29 @@
+package org.usfirst.frc.team2449.robot.subsystems;
+
+import com.ctre.CANTalon.TalonControlMode;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class DriveTrain extends Subsystem {
+
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+	
+	public void basicDrive(double leftPower, double rightPower) {
+		//Basic Driving using direct power mapping
+	}
+	
+	public void changeControlMode(TalonControlMode controlMode) {
+		//Change Talon SRX Control Mode
+	}
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+    
+}
+
