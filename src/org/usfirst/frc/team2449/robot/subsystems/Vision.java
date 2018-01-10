1ccdf83709b586abf9e98384b234fa4f85d49e99
Alex <nerdonator99@gmail.com>
2018-01-10 16:58:41 -0700

    Adds Vision Framework

---
+++ src/org/usfirst/frc/team2449/robot/subsystems/Vision.java
@@ -0,0 +1,33 @@
+package org.usfirst.frc.team2449.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class Vision extends Subsystem {
+
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+	
+	public void startVisionServer() {
+	//start the Vision Server	
+	}
+	
+	public void getVisionTarget() {
+	//get the location of the vision target
+	//NEEDS TO RETURN A VALUE AT SOME POINT
+	}
+	
+	public void getTargetColor(Object visiontargetlocation) {
+	//get the color of the switch lights surrounding the vision target	
+	}
+	
+	
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
+
