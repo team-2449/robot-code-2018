d60b95fccde3b356dc4020cb9569be2596a68799
Alex Manning <nerdonator99@gmail.com>
2018-01-11 16:09:58 -0700

    Changes DriveTrain to support CTRE Phoenix

--- src/org/usfirst/frc/team2449/robot/subsystems/DriveTrain.java
+++ src/org/usfirst/frc/team2449/robot/subsystems/DriveTrain.java
@@ -2,8 +2,8 @@ package org.usfirst.frc.team2449.robot.subsystems;
 
 import org.usfirst.frc.team2449.robot.RobotMap;
 
-import com.ctre.CANTalon;
-import com.ctre.CANTalon.TalonControlMode;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
 
@@ -14,25 +14,20 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 public class DriveTrain extends Subsystem {
 
 	
-	CANTalon left1Talon = new CANTalon(RobotMap.left1TalonPort);
-	CANTalon left2Talon = new CANTalon(RobotMap.left2TalonPort);
-	CANTalon right1Talon = new CANTalon(RobotMap.right1TalonPort);
-	CANTalon right2Talon = new CANTalon(RobotMap.right2TalonPort);
+	TalonSRX left1Talon = new TalonSRX(RobotMap.left1TalonPort);
+	TalonSRX left2Talon = new TalonSRX(RobotMap.left2TalonPort);
+	TalonSRX right1Talon = new TalonSRX(RobotMap.right1TalonPort);
+	TalonSRX right2Talon = new TalonSRX(RobotMap.right2TalonPort);
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 	
 	public void basicDrive(double leftPower, double rightPower) {
 		//Basic Driving using direct power mapping
-		left1Talon.set(-leftPower);
-		left2Talon.set(-leftPower);
-		right1Talon.set(rightPower);
-		right2Talon.set(rightPower);
+		left1Talon.set(ControlMode.PercentOutput, -leftPower);
+		left2Talon.set(ControlMode.PercentOutput, -leftPower);
+		right1Talon.set(ControlMode.PercentOutput, rightPower);
+		right2Talon.set(ControlMode.PercentOutput, rightPower);
 		
-	}
-	
-	
-	public void changeControlMode(TalonControlMode controlMode) {
-		//Change Talon SRX Control Mode
 	}
 
     public void initDefaultCommand() {
