27ae2e4a77a9438441db4bcd913260af85db16c4
hiimgrillj <32688338+hiimgrillj@users.noreply.github.com>
2018-01-11 19:06:56 -0700

    Real Added new Talons

--- src/org/usfirst/frc/team2449/robot/subsystems/DriveTrain.java
+++ src/org/usfirst/frc/team2449/robot/subsystems/DriveTrain.java
@@ -14,37 +14,24 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 public class DriveTrain extends Subsystem {
 
 	
-<<<<<<< HEAD
-	CANTalon left1Talon = new CANTalon(RobotMap.left1TalonPort);
-	CANTalon left2Talon = new CANTalon(RobotMap.left2TalonPort);
-	CANTalon left3Talon = new CANTalon(RobotMap.left3TalonPort);
-	CANTalon right1Talon = new CANTalon(RobotMap.right1TalonPort);
-	CANTalon right2Talon = new CANTalon(RobotMap.right2TalonPort);
-	CANTalon right3Talon = new CANTalon(RobotMap.right3TalonPort);
-=======
 	TalonSRX left1Talon = new TalonSRX(RobotMap.left1TalonPort);
 	TalonSRX left2Talon = new TalonSRX(RobotMap.left2TalonPort);
+	TalonSRX left3Talon = new TalonSRX(RobotMap.left3TalonPort);
 	TalonSRX right1Talon = new TalonSRX(RobotMap.right1TalonPort);
 	TalonSRX right2Talon = new TalonSRX(RobotMap.right2TalonPort);
->>>>>>> d60b95fccde3b356dc4020cb9569be2596a68799
+	TalonSRX right3Talon = new TalonSRX(RobotMap.right3TalonPort);
+
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 	
 	public void basicDrive(double leftPower, double rightPower) {
 		//Basic Driving using direct power mapping
-<<<<<<< HEAD
-		left1Talon.set(-leftPower);
-		left2Talon.set(-leftPower);
-		left3Talon.set(-leftPower);
-		right1Talon.set(rightPower);
-		right2Talon.set(rightPower);
-		right3Talon.set(rightPower);
-=======
+
 		left1Talon.set(ControlMode.PercentOutput, -leftPower);
 		left2Talon.set(ControlMode.PercentOutput, -leftPower);
 		right1Talon.set(ControlMode.PercentOutput, rightPower);
 		right2Talon.set(ControlMode.PercentOutput, rightPower);
->>>>>>> d60b95fccde3b356dc4020cb9569be2596a68799
+
 		
 	}
 
