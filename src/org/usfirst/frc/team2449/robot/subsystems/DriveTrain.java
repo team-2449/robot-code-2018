9e87715b9e0da8b855c157e790811296de32cab2
Alex Manning <nerdonator99@gmail.com>
2018-01-25 15:48:21 -0700

    Changes PID Velocity control to a subsystem function instead of a command/

--- src/org/usfirst/frc/team2449/robot/subsystems/DriveTrain.java
+++ src/org/usfirst/frc/team2449/robot/subsystems/DriveTrain.java
@@ -1,12 +1,14 @@
 package org.usfirst.frc.team2449.robot.subsystems;
 
 
+import org.usfirst.frc.team2449.robot.Robot;
 import org.usfirst.frc.team2449.robot.RobotMap;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.PIDSourceType;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
@@ -24,6 +26,9 @@ public class DriveTrain extends Subsystem {
 	TalonSRX right3Talon = new TalonSRX(RobotMap.right3TalonPort);
 	public PIDDriver leftPIDGroup = new PIDDriver(left1Talon,left2Talon,left3Talon);
 	public PIDDriver rightPIDGroup = new PIDDriver(right1Talon,right2Talon,right3Talon);
+	public PIDController leftPIDController = new PIDController(RobotMap.driveTrainVelocitykP,RobotMap.driveTrainVelocitykI,RobotMap.driveTrainVelocitykD,RobotMap.driveTrainVelocitykF,Robot.robotSensors.left1Encoder,leftPIDGroup);
+	public PIDController rightPIDController = new PIDController(RobotMap.driveTrainVelocitykP,RobotMap.driveTrainVelocitykI,RobotMap.driveTrainVelocitykD,RobotMap.driveTrainVelocitykF,Robot.robotSensors.right1Encoder,rightPIDGroup);
+	
 	public DriveTrain() {
 		right1Talon.setInverted(true);
 		right2Talon.setInverted(true);
@@ -41,6 +46,23 @@ public class DriveTrain extends Subsystem {
 		right1Talon.set(ControlMode.PercentOutput, rightPower);
 		right2Talon.set(ControlMode.PercentOutput, rightPower);
 		right3Talon.set(ControlMode.PercentOutput, rightPower);
+	}
+	
+	public void setVelocity(double leftRate, double rightRate) {
+		leftPIDController.setSetpoint(leftRate);
+    	rightPIDController.setSetpoint(rightRate);
+	}
+	
+	public void enableVelocityControl() {
+		Robot.robotSensors.left1Encoder.setPIDSourceType(PIDSourceType.kRate);
+    	Robot.robotSensors.right1Encoder.setPIDSourceType(PIDSourceType.kRate);
+    	leftPIDController.enable();
+    	rightPIDController.enable();
+	}
+	
+	public void disableVelocityControl() {
+		leftPIDController.disable();
+    	rightPIDController.disable();
 	}
 
     public void initDefaultCommand() {
